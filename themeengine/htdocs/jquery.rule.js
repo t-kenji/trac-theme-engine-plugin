ec545c4317928e8224c551f85f79880b5acdec11
rjollos <rjollos@7322e99d-02ea-0310-aa39-e9a107903beb>
2015-11-01 23:46:13 +0000

    2.2.2dev: Update `jquery.rule.js` to 1.0.2.
    Minified with `uglifyjs --compress -o jquery.rule-min.js -- jquery.rule.js`.
    
    Fixes #12549.
    
    git-svn-id: https://trac-hacks.org/svn/themeengineplugin/trunk@15010 7322e99d-02ea-0310-aa39-e9a107903beb

--- themeengine/htdocs/jquery.rule.js
+++ themeengine/htdocs/jquery.rule.js
@@ -1,12 +1,12 @@
-/**
+/*!
  * jQuery.Rule - Css Rules manipulation, the jQuery way.
- * Copyright (c) 2007-2008 Ariel Flesler - aflesler(at)gmail(dot)com | http://flesler.blogspot.com
+ * Copyright (c) 2007-2011 Ariel Flesler - aflesler(at)gmail(dot)com | http://flesler.blogspot.com
  * Dual licensed under MIT and GPL.
- * Date: 02/27/2008
- * Compatible with jQuery 1.2.x, tested on FF 2, Opera 9, Safari 3, and IE 6, on Windows.
+ * Date: 02/7/2011
+ * Compatible with jQuery 1.2+
  *
  * @author Ariel Flesler
- * @version 1.0.1
+ * @version 1.0.2
  *
  * @id jQuery.rule
  * @param {Undefined|String|jQuery.Rule} The rules, can be a selector, or literal CSS rules. Many can be given, comma separated.
@@ -23,8 +23,8 @@
  *
  * @example var text = $.rule('#screen h2').add('h4').end().eq(4).text();
  */
-;(function( $ ){
-
+;(function( $ ){	
+	
    /**
 	* Notes
 	*	Some styles and animations might fail, please report it.
@@ -35,18 +35,18 @@
 	*	This plugin adds $.rule and also 4 methods to $.fn: ownerNode, sheet, cssRules and cssText
 	*	Note that rules are not directly inside nodes, you need to do: $('style').sheet().cssRules().
 	*/
-
+	
 	var storageNode = $('<style rel="alternate stylesheet" type="text/css" />').appendTo('head')[0],//we must append to get a stylesheet
 		sheet = storageNode.sheet ? 'sheet' : 'styleSheet',
 		storage = storageNode[sheet],//css rules must remain in a stylesheet for IE and FF
 		rules = storage.rules ? 'rules' : 'cssRules',
 		remove = storage.deleteRule ? 'deleteRule' : 'removeRule',
-		owner = storage.ownerNode ? 'ownerNode' : 'owningElement',
+		owner = storage.ownerNode ? 'ownerNode' : 'owningElement',		
 		reRule = /^([^{]+)\{([^}]*)\}/m,
-		reStyle = /([^:]+):([^;}]+)/;
+		reStyle = /([^:]+):([^;}]+)/;	
 
-	storage.disabled = true;//let's ignore your rules
-
+	storage.disabled = true;//let's ignore your rules 
+	
 	var $rule = $.rule = function( r, c ){
 		if(!(this instanceof $rule))
 			return new $rule( r, c );
@@ -54,12 +54,16 @@
 		this.sheets = $rule.sheets(c);
 		if( r && reRule.test(r) )
 			r = $rule.clean( r );
-		if( typeof r == 'object' && !r.exec )
-			return this.setArray( r.get ? r.get() : r.splice ? r : [r] );
-		this.setArray( this.sheets.cssRules().get() );
-		return r ? this.filter( r ) : this;
+		if( typeof r == 'object' && !r.exec ) {
+			setArray( this, r.get ? r.get() : r.splice ? r : [r] );
+		} else {
+			setArray( this, this.sheets.cssRules().get() );
+			if (r)
+				return this.filter( r );
+		}
+		return this;
 	};
-
+	
 	$.extend( $rule, {
 		sheets:function( c ){
 			var o = c;
@@ -89,13 +93,13 @@
 			var p;
 			if( !skip && (p = this.parent(r)) )//if this is an actual rule, and it's appended.
 				r = this.remove( r, p );
-
-			var rule = this.rule( r );
+				
+			var rule = this.rule( r );			
 			if( ss.addRule )
 				ss.addRule( rule[1], rule[2]||';' );//IE won't allow empty rules
 			else if( ss.insertRule )
 				ss.insertRule( rule[1] + '{'+ rule[2] +'}', ss[rules].length );
-
+			
 			return ss[rules][ ss[rules].length - 1 ];//return the added/parsed rule
 		},
 		remove:function( r, p ){
@@ -122,14 +126,14 @@
 			var par;
 			this.sheets().each(function(){
 				if( $.inArray(r, this[rules]) != -1 ){
-					par = this;
+					par = this;	
 					return false;
 				}
 			});
 			return par;
 		},
 		outerText:function( rule ){
-			return !rule ? '' : [rule.selectorText+'{', '\t'+rule.style.cssText,'}'].join('\n').toLowerCase();
+			return !rule || !rule.selectorText ? '' : [rule.selectorText+'{', '\t'+rule.style.cssText,'}'].join('\n').toLowerCase();
 		},
 		text:function( rule, txt ){
 			if( txt !== undefined )
@@ -137,7 +141,7 @@
 			return !rule ? '' : rule.style.cssText.toLowerCase();
 		}
 	});
-
+	
 	$rule.fn = $rule.prototype = {
 		pushStack:function( rs, sh ){
 			var ret = $rule( rs, sh || this.sheets );
@@ -153,7 +157,8 @@
 			if( s.split ){
 				o = $.trim(s).toLowerCase().split(/\s*,\s*/);
 				s = function(){
-					return !!$.grep( this.selectorText.toLowerCase().split(/\s*,\s*/), function( sel ){
+					var s = this.selectorText || '';
+					return !!$.grep( s.toLowerCase().split(/\s*,\s*/), function( sel ){
 						return $.inArray( sel, o ) != -1;
 					}).length;
 				};
@@ -166,7 +171,7 @@
 			}));
 		},
 		add:function( rs, c ){
-			return this.pushStack( $.merge(this.get(), $rule(rs, c)) );
+			return this.pushStack( $.merge(this.get(), $rule(rs, c)) );	
 		},
 		is:function( s ){
 			return !!(s && this.filter( s ).length);
@@ -190,10 +195,10 @@
 				: this.each(function(){	$rule.text( this, txt ); });
 		},
 		outerText:function(){
-			return $rule.outerText(this[0]);
+			return $rule.outerText(this[0]);	
 		}
 	};
-
+	
 	$.each({
 		ownerNode:owner,//when having the stylesheet, get the node that contains it
 		sheet:sheet, //get the stylesheet from the node
@@ -206,13 +211,13 @@
 			});
 		};
 	});
-
+	
 	$.fn.cssText = function(){
 		return this.filter('link,style').eq(0).sheet().cssRules().map(function(){
-			return $rule.outerText(this);
+			return $rule.outerText(this);							   
 		}).get().join('\n');
 	};
-
+	
 	$.each('remove,appendTo,parent'.split(','),function( k, f ){
 		$rule.fn[f] = function(){
 			var args = $.makeArray(arguments), that = this;
@@ -223,19 +228,25 @@
 			});
 		};
 	});
-
-	$.each(('each,index,setArray,get,size,eq,slice,map,attr,andSelf,css,show,hide,toggle,'+
+		
+	$.each(('each,index,get,size,eq,slice,map,attr,andSelf,css,show,hide,toggle,'+
 			'queue,dequeue,stop,animate,fadeIn,fadeOut,fadeTo').split(','),function( k, f ){
-		$rule.fn[f] = $.fn[f];
+		$rule.fn[f] = $.fn[f];																				  
 	});
-
+	
+	// this function has been pulled in from jQuery 1.4.1, because it is an internal function and has been dropped as of 1.4.2
+	function setArray(rule, elems) { 
+		rule.length = 0;
+		Array.prototype.push.apply( rule, elems );
+	}
+	
 	var curCSS = $.curCSS;
 	$.curCSS = function( e, a ){//this hack is still quite exprimental
 		return ('selectorText' in e ) ?
 			e.style[a] || $.prop( e, a=='opacity'? 1 : 0,'curCSS', 0, a )//TODO: improve these defaults
 		: curCSS.apply(this,arguments);
 	};
-
+	
 	/**
 	 * Time to hack jQuery.data for animations.
 	 * Only IE really needs this, but to keep the behavior consistent, I'll hack it for all browsers.
@@ -249,14 +260,14 @@
 			var id = elm.selectorText;
 			if( id )
 				arguments[0] = $rule.cache[id] = $rule.cache[id] || {};
-			return original.apply( $, arguments );
+			return original.apply( $, arguments );	
 		};
 	};
 	$.data = mediator( $.data );
 	$.removeData = mediator( $.removeData );
-
+	
 	$(window).unload(function(){
 		$(storage).cssRules().remove();//empty our rules bin
 	});
-
+		
 })( jQuery );
